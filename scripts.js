a73499d8bb1fa31066f42021e224f8a287191d40
Dan Kiss <flowfast47@gmail.com>
2018-01-27 15:38:53 -0800

    update redux to have reducer and use subscribe for state changes

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@
 const chorusString = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye";
 
 const chorusArray = chorusString.split(', ');
-console.log(chorusArray)
 
 // initial redux state
 const initialState = {
@@ -12,9 +11,9 @@ const initialState = {
   currentPhrase: chorusArray[0],
 }
 
-console.log(initialState);
+// console.log(initialState);
 
-//reducer here
+//redux reducer
 const reducer = (state = initialState, action) => {
   let newState;
   switch (action.type) {
@@ -50,11 +49,24 @@ expect(
 
 // redux store
 const { createStore } = Redux;
-// uses createStore from above
 const store = createStore(reducer);
 console.log(store.getState());
+
+// rendering state in DOM not its html5 not react app render
+const render = () => {
+  document.getElementById('words').innerHTML = store.getState().currentPhrase;
+}
+
+window.onload=function() {
+  render();
+}
 
 // click listener
 const userClick = () => {
   console.log('click');
+  store.dispatch({ type: 'NEXT_LYRIC'} );
+  console.log(store.getState());
 }
+
+// subscribe to redux store
+store.subscribe(render);
